e0a6a54709a3ba92e2ed1a156b1f45482c897fd8
Jashan-panwa <114824486+Jashan-panwa@users.noreply.github.com>
2024-01-31 21:14:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const wrapper=document.querySelector('.wrapper');
+const question=document.querySelector('.question');
+const yesbtn=document.querySelector('.yes-btn');
+const nobtn=document.querySelector('.no-btn');
+
+const wrapperbox=wrapper.getBoundingClientRect();
+const nobtnbox=nobtn.getBoundingClientRect();
+
+const loveIcon = document.querySelector('i');
+
+yesbtn.addEventListener('click',()=>{
+    question.innerHTML=':) Me Too !!!';
+    loveIcon.classList.remove('fa-solid', 'fa-circle-question','doubt');
+    loveIcon.classList.add('fa-regular', 'fa-face-grin-hearts','love');
+
+});
+
+nobtn.addEventListener('mouseover',()=>{
+    const i =Math.floor(Math.random()*(wrapperbox.width - nobtnbox.width))+1;
+    const j =Math.floor(Math.random()*(wrapperbox.height - nobtnbox.height))+1;
+    nobtn.style.left=i+'px';
+    nobtn.style.top=j+'px';
+});
